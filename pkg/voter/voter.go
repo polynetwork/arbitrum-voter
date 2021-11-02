a310453a4c4ba8c4ee957b1514cb09be617d47bf
zhiqiangxu <652732310@qq.com>
2021-11-02 11:24:02 +0800

    add waitTx (#5)
    * fix readme
    
    * add BlocksToWait
    
    * add waitTx

--- pkg/voter/voter.go
+++ pkg/voter/voter.go
@@ -22,6 +22,7 @@ package voter
 import (
 	"context"
 	"encoding/hex"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"time"
@@ -36,6 +37,7 @@ import (
 	"github.com/polynetwork/eth-contracts/go_abi/eccm_abi"
 	sdk "github.com/polynetwork/poly-go-sdk"
 	"github.com/polynetwork/poly/common"
+	"github.com/polynetwork/poly/core/types"
 	common2 "github.com/polynetwork/poly/native/service/cross_chain_manager/common"
 	autils "github.com/polynetwork/poly/native/service/utils"
 )
@@ -56,6 +58,10 @@ func New(polySdk *sdk.PolySdk, signer *sdk.Account, conf *config.Config) *Voter 
 }
 
 func (v *Voter) init() (err error) {
+
+	if v.conf.ArbConfig.BlocksToWait > ARB_USEFUL_BLOCK_NUM {
+		ARB_USEFUL_BLOCK_NUM = v.conf.ArbConfig.BlocksToWait
+	}
 
 	var clients []*ethclient.Client
 	for _, node := range v.conf.ArbConfig.RestURL {
@@ -88,7 +94,7 @@ func (v *Voter) init() (err error) {
 	return
 }
 
-const ARB_USEFUL_BLOCK_NUM = 1
+var ARB_USEFUL_BLOCK_NUM = uint64(1)
 
 func (v *Voter) Start(ctx context.Context) {
 	err := v.init()
@@ -196,15 +202,39 @@ func (v *Voter) fetchLockDepositEvents(height uint64) (err error) {
 			height:  height,
 		}
 
-		_, err = v.commitVote(uint32(height), crossTx.value, crossTx.txId)
+		var txHash string
+		txHash, err = v.commitVote(uint32(height), crossTx.value, crossTx.txId)
 		if err != nil {
 			log.Errorf("commitVote failed:%v", err)
+			return
+		}
+		err = v.waitTx(txHash)
+		if err != nil {
+			log.Errorf("waitTx failed:%v", err)
+			return
 		}
 	}
 
 	log.Infof("arb height %d empty: %v", height, empty)
 
 	return
+}
+
+func (v *Voter) waitTx(txHash string) (err error) {
+	start := time.Now()
+	var tx *types.Transaction
+	for {
+		tx, err = v.polySdk.GetTransaction(txHash)
+		if tx == nil || err != nil {
+			if time.Since(start) > time.Minute*5 {
+				err = fmt.Errorf("waitTx timeout")
+				return
+			}
+			time.Sleep(time.Second)
+			continue
+		}
+		return
+	}
 }
 
 func (v *Voter) commitVote(height uint32, value []byte, txhash []byte) (string, error) {
